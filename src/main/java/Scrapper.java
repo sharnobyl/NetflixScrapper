ccbf24b75aac1f5ce94ba006d5fbdd3544baeaa2
sharnobyl <skaz775@aucklanduni.ac.nz>
2022-10-16 01:42:01 +1300

    feat: scrappes the first 40 movies

--- src/main/java/Scrapper.java
+++ src/main/java/Scrapper.java
@@ -1,5 +1,6 @@
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
@@ -13,28 +14,37 @@ public class Scrapper {
 
     private static final String baseUrl = "https://www.comparetv.com.au/streaming-search-library/?ctvcp=1770";
     public static void main(String[] args) {
-        WebClient client = new WebClient();
+        WebClient client = new WebClient(BrowserVersion.CHROME);
         client.getOptions().setJavaScriptEnabled(false);
         client.getOptions().setCssEnabled(false);
         client.getOptions().setUseInsecureSSL(true);
+        client.getCurrentWindow().getJobManager().removeAllJobs();
 
         try {
             HtmlPage page = client.getPage(baseUrl);
+
             List<HtmlElement> items = page.getByXPath("//div[@class='search-content-item']");
             if(items.isEmpty()){
                 System.out.println("No items found");
             }else{
                 for(HtmlElement htmlItem: items){
-                    HtmlAnchor itemAnchor = (HtmlAnchor) htmlItem.getFirstByXPath("/a");
-                    HtmlElement showName = (HtmlElement) htmlItem.getFirstByXPath("/a/p");
-
+                    String url = htmlItem.getFirstChild().getAttributes().getNamedItem("href").getTextContent();
+                    String temp = htmlItem.asNormalizedText();
+                    int iend = temp.indexOf("\n");
+                    StringBuilder title = new StringBuilder();
+                    for(int i = 0; i < iend; i++){
+                        title.append(temp.charAt(i));
+                    }
                     Item item = new Item();
-                    item.setTitle(itemAnchor.asNormalizedText());
-                    item.setUrl(itemAnchor.getHrefAttribute());
+                    item.setTitle(title.toString());
+                    item.setUrl(url);
+
 
                     ObjectMapper mapper = new ObjectMapper();
+                    String jsonString = mapper.writeValueAsString(item);
 
-
+                    System.out.println(jsonString);
+                    client.close();
                 }
             }
         } catch (IOException e) {
